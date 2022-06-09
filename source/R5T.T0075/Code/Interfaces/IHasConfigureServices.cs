c9cd60ce0b413eab6a0c18918d2ba5d0940ce53e
davidcoats <david@getsafetycone.com>
2022-06-09 12:39:29 -0700

    Interim changes.

--- source/R5T.T0075/Code/Interfaces/IHasConfigureServices.cs
+++ source/R5T.T0075/Code/Interfaces/IHasConfigureServices.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Hosting;
 namespace R5T.T0075
 {
     /// <remarks>
-    /// Same name as <see cref="T011.IHasConfigureServices{TServicesBuilder}"/>, but this includes the <see cref="HostBuilderContext"/>.
+    /// Same name as T011.IHasConfigureServices{TServicesBuilder}, but this includes the <see cref="HostBuilderContext"/>.
     /// </remarks>
     public interface IHasConfigureServices<out TServicesBuilder>
         where TServicesBuilder : IHasConfigureServices<TServicesBuilder>
